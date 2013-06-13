07217a5d4cf7b38617ac344ba2ae89828d93b406
Elliot Sadlon <esadlon@computersoftwareinc.com>
2013-06-13 14:51:57 -0400

    touch up some typos

--- naive_bayes.sql
+++ naive_bayes.sql
@@ -1,5 +1,5 @@
 
--- create extension tablefun;
+-- create extension tablefunc;
 
 create table "SMSSpamCollection"
 (
@@ -10,11 +10,86 @@ create table "SMSSpamCollection"
 )
 ;
 
-\copy "SMSSpamCollection" from 'SMSSpamCollection'
+\copy "SMSSpamCollection" (category, message) from 'SMSSpamCollection'
 
 create index "idx_SMSSpamCollection_message"
 on "SMSSpamCollection"
 using gin(to_tsvector('english', message))
+;
+
+-- ---------------------------------------------------------------- --
+
+create or replace function test_naive_bayes2(decimal)
+returns table (correct bigint, incorrect bigint)
+as
+$body$
+
+  update "SMSSpamCollection"
+  set for_training = case when random() < $1 then true else false end
+  ;
+
+  with document_words
+  as
+  (
+    select for_training,
+	   category,
+	   message_id,
+	   ts_lexemes(to_tsvector('english', message)) as word
+    from "SMSSpamCollection"
+  ),
+
+  word_probs
+  as
+  (
+    select category,
+	   word,
+	   count(*) as n
+    from document_words
+    where for_training
+    group by category,
+	     word
+  ),
+
+  class_probs
+  as
+  (
+    select category,
+	   count(*) as n,
+	   count(*) over () as "N"
+    from document_words
+    where for_training
+    group by category
+  ),
+
+  scores
+  as
+  (
+    select a.message_id,
+	   a.category as original_category,
+	   c.category as predictor_category,
+	   count(*) as num_words,
+	   row_number() over (partition by message_id order by ln(c.n / c."N") * sum(ln(b.n::decimal)) - (count(*) * ln(c.n)) desc) as rank
+    from document_words a
+	   join word_probs b
+	     on a.word = b.word
+	   join class_probs c
+	     on b.category = c.category
+    where not a.for_training
+    group by a.message_id,
+	     a.category,
+	     c.category,
+	     c.n,
+	     c."N"
+  )
+  select count(case when original_category = predictor_category then 1 end) as correct,
+	 count(case when original_category != predictor_category then 1 end) as incorrect
+  from scores
+  where rank = 1
+  ;
+
+$body$
+language sql
+volatile
 ;
 
 -- ---------------------------------------------------------------- --
