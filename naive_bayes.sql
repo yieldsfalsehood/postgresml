35d3d10f3de964eefb6d27204d4b55d8d0359d69
Elliot Sadlon <esadlon@computersoftwareinc.com>
2013-06-17 15:12:34 -0400

    re-write test_naive_bayes2

--- naive_bayes.sql
+++ naive_bayes.sql
@@ -19,81 +19,6 @@ using gin(to_tsvector('english', message))
 
 -- ---------------------------------------------------------------- --
 
-create or replace function test_naive_bayes2(decimal)
-returns table (correct bigint, incorrect bigint)
-as
-$body$
-
-  update "SMSSpamCollection"
-  set for_training = case when random() < $1 then true else false end
-  ;
-
-  with document_words
-  as
-  (
-    select for_training,
-	   category,
-	   message_id,
-	   ts_lexemes(to_tsvector('english', message)) as word
-    from "SMSSpamCollection"
-  ),
-
-  word_probs
-  as
-  (
-    select category,
-	   word,
-	   count(*) as n
-    from document_words
-    where for_training
-    group by category,
-	     word
-  ),
-
-  class_probs
-  as
-  (
-    select category,
-	   count(*) as n,
-	   count(*) over () as "N"
-    from document_words
-    where for_training
-    group by category
-  ),
-
-  scores
-  as
-  (
-    select a.message_id,
-	   a.category as original_category,
-	   c.category as predictor_category,
-	   count(*) as num_words,
-	   row_number() over (partition by message_id order by ln(c.n / c."N") * sum(ln(b.n::decimal)) - (count(*) * ln(c.n)) desc) as rank
-    from document_words a
-	   join word_probs b
-	     on a.word = b.word
-	   join class_probs c
-	     on b.category = c.category
-    where not a.for_training
-    group by a.message_id,
-	     a.category,
-	     c.category,
-	     c.n,
-	     c."N"
-  )
-  select count(case when original_category = predictor_category then 1 end) as correct,
-	 count(case when original_category != predictor_category then 1 end) as incorrect
-  from scores
-  where rank = 1
-  ;
-
-$body$
-language sql
-volatile
-;
-
--- ---------------------------------------------------------------- --
-
 create or replace function test_naive_bayes(decimal)
 returns table (correct bigint, incorrect bigint)
 as
@@ -152,13 +77,11 @@ $body$
     select a.message_id,
            a.category,
            case when (select ln(count(case when c.category = 'spam' then 1 end)::decimal/count(case when c.category = 'ham' then 1 end))
-                      from "SMSSpamCollection" c) + sum(ln(b.p_spam / b.p_ham)) > 0 then 'spam' else 'ham' end as predicted
+                      from "SMSSpamCollection" c) + sum(ln(coalesce(b.p_spam, 0) + 1 / coalesce(b.p_ham, 0) + 1)) > 0 then 'spam' else 'ham' end as predicted
     from "SMSSpamCollection" a
            join word_probs b
              on to_tsquery('english', b.lexeme) @@ to_tsvector('english', a.message)
-    where b.p_ham > 0.0
-      and b.p_spam > 0.0
-      and not a.for_training
+    where not a.for_training
     group by a.message_id,
              a.category
   ) t
@@ -170,3 +93,142 @@ volatile
 ;
 
 -- ---------------------------------------------------------------- --
+
+create or replace function test_naive_bayes2()
+returns table (correct bigint, incorrect bigint)
+as
+$body$
+
+  with
+  scores
+  as
+  (
+    select a.message_id,
+	   a.category as original_category,
+	   c.category as predictor_category,
+	   row_number() over (partition by message_id order by ln(c.n::decimal/c."N") + sum(a.n::decimal * ln(b.n::decimal/b."N")) desc) as rank
+    from document_words a
+	   join word_probs b
+	     on a.word = b.word
+	   join class_probs c
+	     on b.category = c.category
+    where not a.for_training
+    group by a.message_id,
+	     a.category,
+	     c.category,
+	     c.n,
+	     c."N"
+  )
+  select count(case when original_category = predictor_category then 1 end) as correct,
+	 count(case when original_category != predictor_category then 1 end) as incorrect
+  from scores
+  where rank = 1
+  ;
+
+$body$
+language sql
+volatile
+;
+
+-- ---------------------------------------------------------------- --
+
+create table document_words
+as
+    select for_training,
+	   category,
+	   message_id,
+	   (ts_lexemes(to_tsvector('english', message))).lexeme as word,
+	   (ts_lexemes(to_tsvector('english', message))).n
+    from "SMSSpamCollection"
+;
+
+create table vocabulary
+as
+    select distinct
+           word
+    from document_words
+;
+
+create table class_probs
+as
+    select category,
+	   count(*) as n,
+	   sum(count(*)) over () as "N"
+    from "SMSSpamCollection"
+    where for_training
+    group by category
+;
+
+create table word_probs
+as
+    select c.category,
+           b.word,
+	   (coalesce(sum(a.n), 0) + 1)::decimal as n,
+	   c.n as "N"
+    from vocabulary b
+           cross join (select category,
+	                     sum(n + 1) as n
+		      from document_words
+		      where for_training
+		      group by category) c
+	   left join document_words a
+	     on a.word = b.word
+             and a.category = c.category
+	     and a.for_training
+    group by c.category,
+             b.word,
+	     c.n
+;
+
+create index idx_document_words_message_id_testing
+on document_words (message_id)
+where not for_training
+;
+
+create index idx_document_words_word_id_testing
+on document_words (word)
+where not for_training
+;
+
+create index idx_document_words_for_training
+on document_words (for_training)
+;
+
+create index idx_word_probs_word
+on word_probs (word)
+;
+
+update "SMSSpamCollection"
+set for_training = (random() < 0.30)
+;
+
+truncate table class_probs;
+insert into class_probs
+    select category,
+     count(*) as n,
+     sum(count(*)) over () as "N"
+    from "SMSSpamCollection"
+    where for_training
+    group by category
+;
+
+truncate table word_probs;
+insert into word_probs
+    select c.category,
+           b.word,
+           coalesce(sum(a.n), 0)::decimal + 0.10 as n,
+           c.n as "N"
+    from vocabulary b
+           cross join (select category,
+                              sum(n + 0.10) as n
+                       from document_words
+                       where for_training
+                       group by category) c
+         left join document_words a
+           on a.word = b.word
+           and a.category = c.category
+           and a.for_training
+    group by c.category,
+             b.word,
+             c.n
+;
